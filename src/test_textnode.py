1057df5848ffeb4752145beae4b5627059dda71d
Honza Maly <jan@invenus.dev>
2025-03-03 12:30:42 +0100

    feat: added tests to textnode

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD)
+        node2 = TextNode("Hello, World!", TextType.BOLD)
+        self.assertEqual(node1, node2)
+
+    def test_noteq_text(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD)
+        node2 = TextNode("Hello, World World!", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+    
+    def test_noteq2_type(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD)
+        node2 = TextNode("Hello, World!", TextType.NORMAL)
+        self.assertNotEqual(node1, node2)
+    
+    def test_noteq3_url(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD, "https://www.google.com")
+        node2 = TextNode("Hello, World!", TextType.BOLD, "https://www.dash.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_url_none(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD, "https://www.google.com")
+        node2 = TextNode("Hello, World!", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+
+if __name__ == "__main__":
+    unittest.main()
+        
