9b5c47c652b29042eedd746bb13cdfe9406c0939
Honza Maly <jan@invenus.dev>
2025-03-03 16:04:30 +0100

    feat: htmlnode, tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode("div", "Hello, World!", None, {"class": "container", "id": "main"})
+        self.assertEqual(node.props_to_html(), 'class="container" id="main"')
+    
+    def test_none_props_to_html(self):
+        node = HTMLNode("div", "Hello, World!", None, None)
+        self.assertEqual(node.props_to_html(), '')
+
+    def test_empty_props_to_html(self):
+        node = HTMLNode("div", "Hello, World!", None, {})
+        self.assertEqual(node.props_to_html(), '')
