fb3622504c1b7595d0b2e1dd594b23695feb8dd4
Jan Maly <jan@invenus.dev>
2025-03-11 08:18:21 +0100

    feat: build hooked up for github pages

--- src/generator.py
+++ src/generator.py
@@ -12,7 +12,7 @@ def extract_title(markdown_text):
       return line.lstrip('# ').strip()  
   return None
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
   """Generate a page from a markdown file and a template"""
   print(f"Generating page from {from_path} using {template_path} to {dest_path}")
   
@@ -29,6 +29,9 @@ def generate_page(from_path, template_path, dest_path):
   template_text = template_text.replace('{{ Title }}', title)
   # replace the body in the template
   template_text = template_text.replace('{{ Content }}', html_node.to_html())
+  # replace basepath instances in the template
+  template_text = template_text.replace('href="/', f'href="{basepath}')
+  template_text = template_text.replace('src="/', f'src="{basepath}')
 
   # write the generated page to the destination
   # create directory if not exists
@@ -38,7 +41,7 @@ def generate_page(from_path, template_path, dest_path):
   with open(dest_path, 'w') as f:
     f.write(template_text)
   
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
   """Generate all pages in a directory and its subdirectories"""
   entries = os.listdir(dir_path_content)
   for entry in entries:
@@ -46,8 +49,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     if os.path.isdir(full_path):
       # Recurse into the directory
       full_dest_path = os.path.join(dest_dir_path, entry)
-      generate_pages_recursive(full_path, template_path, full_dest_path)
+      generate_pages_recursive(full_path, template_path, full_dest_path, basepath)
     elif entry.endswith('.md'):
       # Generate the page
       dest_path = os.path.join(dest_dir_path, entry.replace('.md', '.html'))      
-      generate_page(full_path, template_path, dest_path)
+      generate_page(full_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+import sys
 from dirops import delete_dir_contents, copy_dir
 from generator import generate_pages_recursive
 
-def main():    
-    delete_dir_contents("public")
-    copy_dir("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+def main():  
+    basepath = '/'    
+    if len(sys.argv) > 1 and sys.argv[1]:
+        basepath = sys.argv[1]    
+    delete_dir_contents("docs")
+    copy_dir("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
